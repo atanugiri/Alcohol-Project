5477b9a4dfc82e5e7569c908628daad1e5967c81
atanu <atanurkm11@gmail.com>
2023-11-12 02:52:38 -0700

    Script for populating featuretable

--- Feature Extraction/writeToFeaturetableCentralZoneFeatures.m
+++ Feature Extraction/writeToFeaturetableCentralZoneFeatures.m
@@ -13,40 +13,58 @@ endDate = endDate + days(1);
 dataInRange = allDates(allDates.referencetime >= startDate & allDates.referencetime <= endDate, :);
 idList = dataInRange.id;
 
-tableName = 'ghrelin_featuretable';
-
 %% Add new column
 % alterQuery = "ALTER TABLE ghrelin_featuretable " + ...
-%     "ADD COLUMN passing_center text, " + ...
-%     "ADD COLUMN time_in_center text";
+%     "ALTER COLUMN passing_center TYPE text, " + ...
+%     "ALTER COLUMN time_in_center TYPE text";
 % exec(conn, alterQuery);
 
-% Iterate over 'id' values and call the function
+tableName = 'ghrelin_featuretable';
+
 for i = 1:length(idList)
     try
         id = idList(i);
-        [passingCenter, timeInCenter] = centralZoneFeatures(id,0.4);
+        [passingCenter, timeInCenter] = centralZoneFeatures(id, 0.4);
 
-        % Convert NaN values to 'NULL' for text columns
-        passingCenter = num2str(passingCenter);
-        timeInCenter = num2str(timeInCenter);
+        % Convert NaN values to NULL
+        passingCenter = handleNaN(passingCenter);
+        timeInCenter = handleNaN(timeInCenter);
 
-        passingCenter = strrep(passingCenter, 'NaN', 'NULL');
-        timeInCenter = strrep(timeInCenter, 'NaN', 'NULL');
+        % Handle empty values
+        passingCenter = handleEmpty(passingCenter);
+        timeInCenter = handleEmpty(timeInCenter);
 
-        % Handle empty strings
-        if isempty(passingCenter)
-            passingCenter = 'NULL';
-        end
-        if isempty(timeInCenter)
-            timeInCenter = 'NULL';
-        end
+        % Convert text to double precision
+        passingCenter = convertToString(passingCenter);
+        timeInCenter = convertToString(timeInCenter);
 
-       updateQuery = sprintf("UPDATE %s SET passing_center=%s, time_in_center=%s " + ...
+        updateQuery = sprintf("UPDATE %s SET passing_center=%s, time_in_center=%s " + ...
             "WHERE id=%d", tableName, passingCenter, timeInCenter, id);
 
-    catch
-        fprintf("Error in writeToFeaturetable for id = %d\n", id);
+        % Execute the update query here
+        exec(conn,updateQuery);
+
+    catch exception
+        fprintf("Error in writeToFeaturetable for id = %d: %s\n", id, exception.message);
         continue;
+    end
+end
+
+function value = handleNaN(value)
+    if isnan(value)
+        value = 'NULL';
+    end
+end
+
+function value = handleEmpty(value)
+    if isempty(value)
+        value = 'NULL';
+    end
+end
+
+function value = convertToString(value)
+    % Convert to numeric if not NaN or empty
+    if all(~isnan(value)) && all(~isempty(value))
+        value = num2str(value); % Convert to string for uniformity
     end
 end
