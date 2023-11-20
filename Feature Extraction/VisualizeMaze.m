fd45183a6fd0fdf81792a0c0789bc891d8379fe0
atanu <atanurkm11@gmail.com>
2023-11-20 16:48:39 -0700

    visualize maze for normalization

--- Feature Extraction/VisualizeMaze.m
+++ Feature Extraction/VisualizeMaze.m
@@ -6,8 +6,8 @@ conn = database(datasource,'postgres','1234');
 dateQuery = "SELECT id, referencetime FROM live_table ORDER BY id";
 allDates = fetch(conn, dateQuery);
 allDates.referencetime = datetime(allDates.referencetime, 'Format', 'MM/dd/yyyy');
-startDate = datetime('09/12/2023', 'InputFormat', 'MM/dd/yyyy');
-endDate = datetime('10/31/2023', 'InputFormat', 'MM/dd/yyyy');
+startDate = datetime('09/25/2023', 'InputFormat', 'MM/dd/yyyy');
+endDate = datetime('09/27/2023', 'InputFormat', 'MM/dd/yyyy');
 endDate = endDate + days(1);
 
 dataInRange = allDates(allDates.referencetime >= startDate & allDates.referencetime <= endDate, :);
@@ -21,13 +21,13 @@ close(conn);
 
 liveTableData.referencetime = datetime(liveTableData.referencetime, 'Format', 'MM/dd/yyyy');
 liveTableData.mazenumber = string(liveTableData.mazenumber);
-liveTableData.xcoordinates2 = transformPgarray(liveTableData.xcoordinates2);
-liveTableData.ycoordinates2 = transformPgarray(liveTableData.ycoordinates2);
+liveTableData.(4) = transformPgarray(liveTableData.(4));
+liveTableData.(5) = transformPgarray(liveTableData.(5));
 
 plotType = input('Ploy type? (all or session): ', 's');
 if strcmpi(plotType, 'all')
-    x = vertcat(liveTableData.xcoordinates2{:});
-    y = vertcat(liveTableData.ycoordinates2{:});
+    x = vertcat(liveTableData.(3){:});
+    y = vertcat(liveTableData.(4){:});
     plot(x,y,'.');
 end
 
@@ -35,11 +35,11 @@ if strcmpi(plotType, 'session')
     liveTableData.referencetime = string(liveTableData.referencetime);
     dateList = unique(liveTableData.referencetime);
 
-    for date = 5:6 %1:length(dateList)
+    for date = 1:length(dateList)
         dateData = liveTableData(liveTableData.referencetime == dateList(date), :);
         mazeData = cell(1,4);
-        mazeData{1} = dateData(dateData.mazenumber == "maze 1", :);
-        mazeData{2} = dateData(dateData.mazenumber == "maze 2", :);
+        mazeData{1} = dateData(dateData.mazenumber == "maze 2", :);
+        mazeData{2} = dateData(dateData.mazenumber == "maze 1", :);
         mazeData{3} = dateData(dateData.mazenumber == "maze 3", :);
         mazeData{4} = dateData(dateData.mazenumber == "maze 4", :);
 
@@ -47,16 +47,24 @@ if strcmpi(plotType, 'session')
         set(gcf, 'Windowstyle', 'docked');
 
         for maze = 1:4
-            subplot(2,2,maze);
+            if maze == 1
+                subplot(2,2,2);
+            elseif maze == 2
+                subplot(2,2,1);
+            else
+                subplot(2,2,maze);
+            end
+
             hold on;
             currentData = mazeData{maze};
-            x = vertcat(currentData.xcoordinates2{:});
-            y = vertcat(currentData.ycoordinates2{:});
+            x = vertcat(currentData.(4){:});
+            y = vertcat(currentData.(5){:});
             id = currentData.id(1);
             [xNormalized, yNormalized] = coordinateNormalization(x, y, id);
             plot(xNormalized,yNormalized,'k.');
             title(sprintf('Trials: %d',height(currentData)));
-            plotFeeders(maze);
+            
+            mazeMethods(maze,0.25,0.3);
         end % End of maze 1
         sgtitle(sprintf('%s', dateList(date)));
 
@@ -67,7 +75,7 @@ end
 %% Description of transformPgarray
 function transformedData = transformPgarray(pgarrayData)
 
-% pgarrayData = liveTableData.xcoordinates2; % For testing
+% pgarrayData = liveTableData.(3); % For testing
 transformedData = cell(length(pgarrayData),1);
 for idx = 1:length(pgarrayData)
     string_all_xcoordinates = string(pgarrayData(idx,1));
@@ -76,48 +84,4 @@ for idx = 1:length(pgarrayData)
     transformedData{idx,1} = str2double(split_all_xcoordinates);
 end
 
-end
-
-%% Description of plot feeder
-function plotFeeders(maze)
-grayFace = [0.3 0.3 0.3 0.3];
-switch maze
-    case 1
-        rectangle('Position',[-1.05 0.75 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-        rectangle('Position',[-1.05 -0.05 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-        rectangle('Position',[-0.25 -0.05 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-        rectangle('Position',[-0.25 0.75 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-
-    case 2
-        rectangle('Position',[0.75 -0.05 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-        rectangle('Position',[-0.05 -0.05 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-        rectangle('Position',[-0.05 0.75 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-        rectangle('Position',[0.75 0.75 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-    case 3
-        rectangle('Position',[-0.25 -1.05 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-        rectangle('Position',[-0.25 -0.25 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-        rectangle('Position',[-1.05 -0.25 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-        rectangle('Position',[-1.05 -1.05 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-    case 4
-        rectangle('Position',[-0.05 -0.25 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-        rectangle('Position',[0.75 -0.25 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-        rectangle('Position',[0.75 -1.05 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-        rectangle('Position',[-0.05 -1.05 0.3 0.3],'EdgeColor','none', ...
-            'FaceColor',grayFace);
-end
 end
