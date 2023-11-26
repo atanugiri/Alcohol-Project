10ff5f9f52906d597c4e2bebcd9c21058a6d6094
atanu <atanurkm11@gmail.com>
2023-11-26 13:44:06 -0700

    coordinate normalization

--- Feature Extraction/coordinateNormalization.m
+++ Feature Extraction/coordinateNormalization.m
@@ -5,9 +5,8 @@
 
 function [xNormalized, yNormalized] = coordinateNormalization(xCoordinate, yCoordinate, id)
 % loadFile = load('xyData.mat');
-% dataTable = loadFile.subject_data_table_with_tone;
-% xCoordinate = dataTable.xcoordinates2; yCoordinate = dataTable.ycoordinates2;
-% id = 141475;
+% xCoordinate = loadFile.x; yCoordinate = loadFile.y;
+% id = 265406;
 
 % remove nan entries
 badDataWithTone = table(xCoordinate,yCoordinate,'VariableNames',{'X','Y'});
@@ -22,19 +21,19 @@ quadrant1Flag = cleanedData.X >= 0 & cleanedData.Y >= 0;
 quadrant2Flag = cleanedData.X <= 0 & cleanedData.Y >= 0;
 quadrant3Flag = cleanedData.X <= 0 & cleanedData.Y <= 0;
 
-if (sum(quadrant1Flag)/length(quadrant1Flag) > 0.9)
+if (sum(quadrant1Flag)/length(quadrant1Flag) > 0.6)
     xNormalized = (xCoordinate - min(xCleanedByYAxis{1}))/...
         (max(xCleanedByYAxis{1}) - min(xCleanedByYAxis{1}));
     yNormalized = (yCoordinate - min(yCleanedByYAxis{1}))/...
         (max(yCleanedByYAxis{1}) - min(yCleanedByYAxis{1}));
 
-elseif (sum(quadrant2Flag)/length(quadrant2Flag) > 0.9)
+elseif (sum(quadrant2Flag)/length(quadrant2Flag) > 0.6)
     xNormalized = (xCoordinate - max(xCleanedByYAxis{2}))/...
         (max(xCleanedByYAxis{2}) - min(xCleanedByYAxis{2}));
     yNormalized = (yCoordinate - min(yCleanedByYAxis{2}))/...
         (max(yCleanedByYAxis{2}) - min(yCleanedByYAxis{2}));
 
-elseif (sum(quadrant3Flag)/length(quadrant3Flag) > 0.9)
+elseif (sum(quadrant3Flag)/length(quadrant3Flag) > 0.6)
     xNormalized = (xCoordinate - max(xCleanedByYAxis{3}))/...
         (max(xCleanedByYAxis{3}) - min(xCleanedByYAxis{3}));
     yNormalized = (yCoordinate - max(yCleanedByYAxis{3}))/...
