cd0b15ea606b2d23066625444574ec1398e3f921
atanu <atanurkm11@gmail.com>
2023-11-29 15:29:49 -0700

    Script for populating featuretable

--- Feature Extraction/writeToFeaturetable.m
+++ Feature Extraction/writeToFeaturetable.m
@@ -4,8 +4,8 @@ conn = database(datasource,'postgres','1234');
 dateQuery = "SELECT id, referencetime FROM live_table ORDER BY id";
 allDates = fetch(conn, dateQuery);
 allDates.referencetime = datetime(allDates.referencetime, 'Format', 'MM/dd/yyyy');
-startDate = datetime('09/12/2023', 'InputFormat', 'MM/dd/yyyy');
-endDate = datetime('10/31/2023', 'InputFormat', 'MM/dd/yyyy');
+startDate = datetime('09/25/2023', 'InputFormat', 'MM/dd/yyyy');
+endDate = datetime('09/25/2023', 'InputFormat', 'MM/dd/yyyy');
 endDate = endDate + days(1);
 
 dataInRange = allDates(allDates.referencetime >= startDate & allDates.referencetime <= endDate, :);
@@ -44,9 +44,9 @@ for index = 1:length(idList)
         logicalApproach3s = convertToString(logicalApproach3s);
         logicalApproach5s = convertToString(logicalApproach5s);
 
-        updateQuery = sprintf("UPDATE %s SET entry_time_20_2nd=%s, " + ...
-            "logical_approach_20_2nd=%s, logical_approach_3s_20_2nd=%s, " + ...
-            "logical_approach_5s_20_2nd=%s WHERE id=%d", tableName, ...
+        updateQuery = sprintf("UPDATE %s SET entry_time=%s, " + ...
+            "logical_approach=%s, logical_approach_1_5s=%s, " + ...
+            "logical_approach_2s=%s WHERE id=%d", tableName, ...
             entryTime, logicalApproach, logicalApproach3s, logicalApproach5s, id);
 
         exec(conn, updateQuery);
