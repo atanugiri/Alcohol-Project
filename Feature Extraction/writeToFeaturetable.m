1e5946541485d2b92897370f24f04d4ec51362f3
atanu <atanurkm11@gmail.com>
2023-11-30 16:26:18 -0700

    Script for populating featuretable

--- Feature Extraction/writeToFeaturetable.m
+++ Feature Extraction/writeToFeaturetable.m
@@ -4,8 +4,8 @@ conn = database(datasource,'postgres','1234');
 dateQuery = "SELECT id, referencetime FROM live_table ORDER BY id";
 allDates = fetch(conn, dateQuery);
 allDates.referencetime = datetime(allDates.referencetime, 'Format', 'MM/dd/yyyy');
-startDate = datetime('06/27/2022', 'InputFormat', 'MM/dd/yyyy');
-endDate = datetime('06/27/2022', 'InputFormat', 'MM/dd/yyyy');
+startDate = datetime('06/28/2022', 'InputFormat', 'MM/dd/yyyy');
+endDate = datetime('06/28/2022', 'InputFormat', 'MM/dd/yyyy');
 endDate = endDate + days(1);
 
 dataInRange = allDates(allDates.referencetime >= startDate & allDates.referencetime <= endDate, :);
