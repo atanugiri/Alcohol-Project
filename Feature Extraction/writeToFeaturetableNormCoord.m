e2ccb088635b196713570449d41feb681c7b4e26
atanu <atanurkm11@gmail.com>
2023-12-11 16:55:01 -0700

    Script for populating featuretable

--- Feature Extraction/writeToFeaturetableNormCoord.m
+++ Feature Extraction/writeToFeaturetableNormCoord.m
@@ -1,3 +1,6 @@
+% Author: Atanu Giri
+% Date: 12/11/2023
+
 % clear; clc;
 datasource = 'live_database';
 conn = database(datasource,'postgres','1234');
@@ -5,7 +8,7 @@ dateQuery = "SELECT id, referencetime FROM live_table ORDER BY id";
 allDates = fetch(conn, dateQuery);
 allDates.referencetime = datetime(allDates.referencetime, 'Format', 'MM/dd/yyyy');
 startDate = datetime('09/12/2023', 'InputFormat', 'MM/dd/yyyy');
-endDate = datetime('10/31/2023', 'InputFormat', 'MM/dd/yyyy');
+endDate = datetime('09/12/2023', 'InputFormat', 'MM/dd/yyyy');
 endDate = endDate + days(1);
 
 dataInRange = allDates(allDates.referencetime >= startDate & allDates.referencetime <= endDate, :);
@@ -18,24 +21,18 @@ for index = 1:length(idList)
     try
         [normT, normX, normY] = extractNormalizedCoordinate(id);
 
-        % Check for NaN values and convert them to NULL
-        normT = handleNaN(normT);
-        normX = handleNaN(normX);
-        normY = handleNaN(normY);
+        norm_t_string = sprintf('ARRAY[%s]', strjoin(cellstr(num2str(normT, '%.6f')), ','));
+        norm_x_string = sprintf('ARRAY[%s]', strjoin(cellstr(num2str(normX, '%.6f')), ','));
+        norm_y_string = sprintf('ARRAY[%s]', strjoin(cellstr(num2str(normY, '%.6f')), ','));
 
-        updateQuery = sprintf("UPDATE %s SET norm_t=%f, norm_x=%f, norm_y=%f " + ...
-            "WHERE id=%d", tableName, normT, normX, normY, id);
+
+        updateQuery = sprintf("UPDATE %s SET norm_t=%s, norm_x=%s, norm_y=%s " + ...
+            "WHERE id=%d", tableName, norm_t_string, norm_x_string, norm_y_string, id);
 
         exec(conn, updateQuery);
 
     catch ME
         fprintf("Calculation error in %d: %s\n", id, ME.message);
         continue;
-    end
-end
-
-function value = handleNaN(value)
-    if isnan(value)
-        value = 'NULL';
     end
 end
