c1b874ae21f8d5b3b73a80e12d983e9d2fd5326c
atanu <atanurkm11@gmail.com>
2023-12-20 21:08:15 -0700

    Feature extraction

---
+++ Feature Extraction/accelerationAndJerkOulierFun.m
@@ -0,0 +1,78 @@
+% Author: Atanu Giri
+% date: 12/20/2023
+
+function [accOutlierMoveMedian,jerkOutlierMoveMedian] = accelerationAndJerkOulierFun(id)
+%
+% This algorithm calculates the number of outliers by analyzing subject's
+% acceleration and jerkness by movemedian method.
+%
+close all; clc;
+
+% id = 265302;
+% make connection with database
+datasource = 'live_database';
+conn = database(datasource,'postgres','1234');
+
+% write query
+query = sprintf("SELECT id, distance_until_limiting_time_stamp, norm_t, norm_x, norm_y " + ...
+    "FROM ghrelin_featuretable WHERE id = %d", id);
+subject_data = fetch(conn,query);
+
+try
+    subject_data.distance_until_limiting_time_stamp = str2double(subject_data.distance_until_limiting_time_stamp);
+    % Accessing PGArray data as double
+    for column = size(subject_data,2) - 2:size(subject_data,2)
+        stringAllRows = string(subject_data.(column));
+        regAllRows = regexprep(stringAllRows,'{|}','');
+        splitAllRows = split(regAllRows,',');
+        doubleData = str2double(splitAllRows);
+        subject_data.(column){1} = doubleData;
+    end
+
+    X = subject_data.norm_x{1};
+    Y = subject_data.norm_y{1};
+    t = subject_data.norm_t{1};
+
+    limitingTimeIndex = find(t == 15);
+    X = X(1:limitingTimeIndex);
+    Y = Y(1:limitingTimeIndex);
+    t = t(1:limitingTimeIndex);
+
+    % Calculate velocity
+    Vx = diff(X) ./ diff(t); % velocity in the X direction
+    Vy = diff(Y) ./ diff(t); % velocity in the Y direction
+
+    % Append a zero at the beginning to make the size of V match the size of t
+    Vx = [0; Vx];
+    Vy = [0; Vy];
+
+    % Calculate acceleration
+    Ax = diff(Vx) ./ diff(t); % acceleration in the X direction
+    Ay = diff(Vy) ./ diff(t); % acceleration in the Y direction
+
+    % Append a zero at the beginning to make the size of A match the size of t
+    Ax = [0; Ax];
+    Ay = [0; Ay];
+
+    % Total acceleration magnitude
+    A = sqrt(Ax.^2 + Ay.^2);
+
+    % Calculate acceleration outlier
+    accOutlierMoveMedian = sum(isoutlier(A,"movmedian",5))/subject_data.distance_until_limiting_time_stamp;
+
+    % Calculate Jerk
+    Jx = diff(Ax) ./ diff(t);
+    Jy = diff(Ay) ./ diff(t);
+
+    Jx = [0; Jx];
+    Jy = [0; Jy];
+
+    % Total acceleration magnitude
+    J = sqrt(Jx.^2 + Jy.^2);
+    jerkOutlierMoveMedian = sum(isoutlier(J,"movmedian",5))/subject_data.distance_until_limiting_time_stamp;
+
+catch
+    sprintf("An error occured for id = %d\n", id);
+end
+
+end
---
+++ Feature Extraction/writeToFeaturetableAccJerk.m
@@ -0,0 +1,62 @@
+% clear; clc;
+datasource = 'live_database';
+conn = database(datasource,'postgres','1234');
+dateQuery = "SELECT id, referencetime FROM live_table ORDER BY id";
+allDates = fetch(conn, dateQuery);
+allDates.referencetime = datetime(allDates.referencetime, 'Format', 'MM/dd/yyyy');
+startDate = datetime('09/12/2023', 'InputFormat', 'MM/dd/yyyy');
+endDate = datetime('10/31/2023', 'InputFormat', 'MM/dd/yyyy');
+endDate = endDate + days(1);
+
+dataInRange = allDates(allDates.referencetime >= startDate & allDates.referencetime <= endDate, :);
+idList = dataInRange.id;
+
+tableName = 'ghrelin_featuretable';
+
+for index = 1:length(idList)
+    id = idList(index);
+    try
+        [accOutlierMoveMedian,jerkOutlierMoveMedian] = accelerationAndJerkOulierFun(id);
+
+        % Convert NaN values to NULL
+        accOutlierMoveMedian = handleNaN(accOutlierMoveMedian);
+        jerkOutlierMoveMedian = handleNaN(jerkOutlierMoveMedian);
+
+        % Handle empty values
+        accOutlierMoveMedian = handleEmpty(accOutlierMoveMedian);
+        jerkOutlierMoveMedian = handleEmpty(jerkOutlierMoveMedian);
+
+        % Convert NaN values to 'NULL' for text columns
+        accOutlierMoveMedian = convertToString(accOutlierMoveMedian);
+        jerkOutlierMoveMedian = convertToString(jerkOutlierMoveMedian);
+
+        updateQuery = sprintf("UPDATE %s SET acc_outlier_move_median=%s, " + ...
+            "jerk_outlier_move_median=%s WHERE id=%d", tableName, ...
+            accOutlierMoveMedian, jerkOutlierMoveMedian, id);
+
+        exec(conn, updateQuery);
+
+    catch ME
+        fprintf("Calculation error in %d: %s\n", id, ME.message);
+        continue;
+    end
+end
+
+function value = handleNaN(value)
+    if isnan(value)
+        value = 'NULL';
+    end
+end
+
+function value = handleEmpty(value)
+    if isempty(value)
+        value = 'NULL';
+    end
+end
+
+function value = convertToString(value)
+    % Convert to numeric if not NaN or empty
+    if all(~isnan(value)) && all(~isempty(value))
+        value = num2str(value); % Convert to string for uniformity
+    end
+end
