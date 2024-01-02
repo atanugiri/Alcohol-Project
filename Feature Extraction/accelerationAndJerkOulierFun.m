7a3e3a8e4eb5b12c3b756731c35be1d92799946a
atanu <atanurkm11@gmail.com>
2024-01-02 13:42:07 -0700

    Feature Extraction

--- Feature Extraction/accelerationAndJerkOulierFun.m
+++ Feature Extraction/accelerationAndJerkOulierFun.m
@@ -14,12 +14,22 @@ datasource = 'live_database';
 conn = database(datasource,'postgres','1234');
 
 % write query
-query = sprintf("SELECT id, distance_until_limiting_time_stamp, norm_t, norm_x, norm_y " + ...
+query = sprintf("SELECT id, distance_until_limiting_time_stamp_old, norm_t, norm_x, norm_y " + ...
     "FROM ghrelin_featuretable WHERE id = %d", id);
 subject_data = fetch(conn,query);
 
+liveTableQuery = sprintf("SELECT id, playstarttrialtone " + ...
+    "FROM live_table WHERE id = %d", id);
+liveTableData = fetch(conn, liveTableQuery);
+
+subject_data = innerjoin(liveTableData, subject_data, 'Keys', 'id');
+
 try
-    subject_data.distance_until_limiting_time_stamp = str2double(subject_data.distance_until_limiting_time_stamp);
+    subject_data.playstarttrialtone = str2double(subject_data.playstarttrialtone);
+    if isnan(subject_data.playstarttrialtone)
+        subject_data.playstarttrialtone = 2;
+    end
+    subject_data.distance_until_limiting_time_stamp_old = str2double(subject_data.distance_until_limiting_time_stamp_old);
     % Accessing PGArray data as double
     for column = size(subject_data,2) - 2:size(subject_data,2)
         stringAllRows = string(subject_data.(column));
@@ -33,10 +43,10 @@ try
     Y = subject_data.norm_y{1};
     t = subject_data.norm_t{1};
 
-    limitingTimeIndex = find(t == 15);
-    X = X(1:limitingTimeIndex);
-    Y = Y(1:limitingTimeIndex);
-    t = t(1:limitingTimeIndex);
+    limitingTimeIndex = 20;
+    X = X(t >= subject_data.playstarttrialtone & t <= limitingTimeIndex);
+    Y = Y(t >= subject_data.playstarttrialtone & t <= limitingTimeIndex);
+    t = t(t >= subject_data.playstarttrialtone & t <= limitingTimeIndex);
 
     % Calculate velocity
     Vx = diff(X) ./ diff(t); % velocity in the X direction
@@ -58,7 +68,7 @@ try
     A = sqrt(Ax.^2 + Ay.^2);
 
     % Calculate acceleration outlier
-    accOutlierMoveMedian = sum(isoutlier(A,"movmedian",5))/subject_data.distance_until_limiting_time_stamp;
+    accOutlierMoveMedian = sum(isoutlier(A,"movmedian",5))/subject_data.distance_until_limiting_time_stamp_old;
 
     % Calculate Jerk
     Jx = diff(Ax) ./ diff(t);
@@ -69,7 +79,7 @@ try
 
     % Total acceleration magnitude
     J = sqrt(Jx.^2 + Jy.^2);
-    jerkOutlierMoveMedian = sum(isoutlier(J,"movmedian",5))/subject_data.distance_until_limiting_time_stamp;
+    jerkOutlierMoveMedian = sum(isoutlier(J,"movmedian",5))/subject_data.distance_until_limiting_time_stamp_old;
 
 catch
     sprintf("An error occured for id = %d\n", id);
