15ee33a0feeea7a640ecc20c6cb62969e00397ef
atanu <atanurkm11@gmail.com>
2024-02-02 16:00:41 -0700

    Feature Extraction

--- Feature Extraction/extractNormalizedCoordinate.m
+++ Feature Extraction/extractNormalizedCoordinate.m
@@ -3,7 +3,7 @@
 
 function [normT, normX, normY] = extractNormalizedCoordinate(id)
 
-% id = 18343;
+% id = 10988;
 
 datasource = 'live_database';
 conn = database(datasource,'postgres','1234');
@@ -12,15 +12,22 @@ conn = database(datasource,'postgres','1234');
 query = sprintf("SELECT id, coordinatetimes2, xcoordinates2, " + ...
     "ycoordinates2 FROM live_table WHERE id = %d", id);
 subject_data = fetch(conn,query);
+% close(conn);
 
 try
     % Accessing PGArray data as double
     for column = size(subject_data,2) - 2:size(subject_data,2)
-        stringAllRows = string(subject_data.(column));
-        regAllRows = regexprep(stringAllRows,'{|}','');
-        splitAllRows = split(regAllRows,',');
-        doubleData = str2double(splitAllRows);
-        subject_data.(column){1} = doubleData;
+        %         stringAllRows = string(subject_data.(column));
+        %         regAllRows = regexprep(stringAllRows,'{|}','');
+        %         splitAllRows = split(regAllRows,',');
+        %         doubleData = str2double(splitAllRows);
+        %         subject_data.(column){1} = doubleData;
+
+        % vectorization approach
+        strData = cellfun(@(x) string(x), subject_data.(column));
+        regData = arrayfun(@(x) regexprep(x,'{|}',''), strData);
+        splitData = arrayfun(@(x) split(x, ','), regData, 'UniformOutput', false);
+        subject_data.(column) = cellfun(@(x) str2double(x), splitData, 'UniformOutput', false);
     end
 
     rawData = table(subject_data.coordinatetimes2{1}, subject_data.xcoordinates2{1}, ...
--- Feature Extraction/writeToFeaturetableNormCoord.m
+++ Feature Extraction/writeToFeaturetableNormCoord.m
@@ -4,19 +4,22 @@
 % clear; clc;
 datasource = 'live_database';
 conn = database(datasource,'postgres','1234');
-dateQuery = "SELECT id, referencetime FROM live_table ORDER BY id";
-allDates = fetch(conn, dateQuery);
-allDates.referencetime = datetime(allDates.referencetime, 'Format', 'MM/dd/yyyy');
-startDate = datetime('09/12/2023', 'InputFormat', 'MM/dd/yyyy');
-endDate = datetime('12/11/2023', 'InputFormat', 'MM/dd/yyyy');
-endDate = endDate + days(1);
+% dateQuery = "SELECT id, referencetime FROM live_table ORDER BY id";
+% allDates = fetch(conn, dateQuery);
+% allDates.referencetime = datetime(allDates.referencetime, 'Format', 'MM/dd/yyyy');
+% startDate = datetime('09/12/2023', 'InputFormat', 'MM/dd/yyyy');
+% endDate = datetime('12/11/2023', 'InputFormat', 'MM/dd/yyyy');
+% endDate = endDate + days(1);
+% 
+% dataInRange = allDates(allDates.referencetime >= startDate & allDates.referencetime <= endDate, :);
+% idList = dataInRange.id;
 
-dataInRange = allDates(allDates.referencetime >= startDate & allDates.referencetime <= endDate, :);
-idList = dataInRange.id;
+loadFile = load("emptyIDs.mat");
+idList = loadFile.emptyIDs;
 
 tableName = 'ghrelin_featuretable';
 
-for index = 1:length(idList)
+parfor index = 1:length(idList)
     id = idList(index);
     try
         [normT, normX, normY] = extractNormalizedCoordinate(id);
