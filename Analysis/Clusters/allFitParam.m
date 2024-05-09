b4fb4310fbc739e8cdf79dbd1b7c283e923568fd
atanu <atanurkm11@gmail.com>
2024-05-09 16:08:24 -0600

    Data analysis

--- Analysis/Clusters/allFitParam.m
+++ Analysis/Clusters/allFitParam.m
@@ -1,64 +1,79 @@
 % Author: Atanu Giri
 % Date: 04/15/2024
 %
+% Possible parameter names 
+% logistic3 fit: 'UA', 'slope', 'shift', 'Rsq'
+% logistic4 fit: 'LA', 'slope', 'shift', 'UA', 'Rsq'
+% BC fit: 'LA', 'slope', 'shift', 'UA', 'param_f', 'Rsq'
+% LC fit: 'UA', 'slope', 'shift', 'UA_2', 'slope_2', 'shift_2', 'Rsq'
+%
 % Example usage: allFitParam('Alcohol_approachavoid_logistic3_fitting_param.mat')
 %
 
 function varargout = allFitParam(varargin)
 
-% varargin = {'P2L1 Alcohol_approachavoid_logistic3_fitting_param.mat'};
+% varargin = {'P2L1 Boost and alcohol_approachavoid_logistic4_fitting_param.mat'};
 
 scriptDir = fileparts(mfilename('fullpath'));
 myPath = fullfile(scriptDir, 'Mat files/');
 
 allRsq = cell(1, numel(varargin));
+UA = cell(1, numel(varargin));
+slope = cell(1, numel(varargin));
+shift = cell(1, numel(varargin));
+animals = cell(1, numel(varargin));
 
 if contains(varargin{1}, 'logistic3')
-    UA = cell(1, numel(varargin));
-    slope = cell(1, numel(varargin));
-    shift = cell(1, numel(varargin));
-
     for grp = 1:numel(varargin)
         loadFile = load(fullfile(myPath, varargin{grp}));
-        [fitParam, R_squared] = helperFun(loadFile);
+        [fitParam, R_squared, animal] = helperFun(loadFile);
+
+        %% Remove negative slopes (Optional)
+        ngtvSlpFltr = fitParam(:,2) <= 0;
+        fitParam = fitParam(~ngtvSlpFltr, :);
+        R_squared = R_squared(~ngtvSlpFltr, :);
+        animal = animal(~ngtvSlpFltr, :);
 
         allRsq{grp} = R_squared;
         UA{grp} = fitParam(:,1);
         slope{grp} = fitParam(:,2);
         shift{grp} = fitParam(:,3);
+        animals{grp} = animal;
     end
 
-    varargout = {UA, slope, shift, allRsq};
+    varargout = {UA, slope, shift, allRsq, animals};
 
 elseif contains(varargin{1}, 'logistic4')
     LA = cell(1, numel(varargin));
-    slope = cell(1, numel(varargin));
-    shift = cell(1, numel(varargin));
-    UA = cell(1, numel(varargin));
 
     for grp = 1:numel(varargin)
         loadFile = load(fullfile(myPath, varargin{grp}));
-        [fitParam, R_squared] = helperFun(loadFile);
+        [fitParam, R_squared, animal] = helperFun(loadFile);
+
+        %% Remove negative slopes (Optional)
+        ngtvSlpFltr = fitParam(:,2) <= 0;
+        fitParam = fitParam(~ngtvSlpFltr, :);
+        R_squared = R_squared(~ngtvSlpFltr, :);
+        animal = animal(~ngtvSlpFltr, :);
 
         allRsq{grp} = R_squared;
         LA{grp} = fitParam(:,1);
         slope{grp} = fitParam(:,2);
         shift{grp} = fitParam(:,3);
         UA{grp} = fitParam(:,4);
+        animals{grp} = animal;
+
     end
 
-    varargout = {LA, slope, shift, UA, allRsq};
+    varargout = {LA, slope, shift, UA, allRsq, animals};
 
 elseif contains(varargin{1}, 'BC')
     LA = cell(1, numel(varargin));
-    slope = cell(1, numel(varargin));
-    shift = cell(1, numel(varargin));
-    UA = cell(1, numel(varargin));
     param_f = cell(1, numel(varargin));
 
     for grp = 1:numel(varargin)
         loadFile = load(fullfile(myPath, varargin{grp}));
-        [fitParam, R_squared] = helperFun(loadFile);
+        [fitParam, R_squared, animal] = helperFun(loadFile);
 
         allRsq{grp} = R_squared;
         LA{grp} = fitParam(:,3);
@@ -71,16 +86,13 @@ elseif contains(varargin{1}, 'BC')
     varargout = {LA, slope, shift, UA, param_f, allRsq};
 
 elseif contains(varargin{1}, 'LC')
-    UA = cell(1, numel(varargin));
-    slope = cell(1, numel(varargin));
-    shift = cell(1, numel(varargin));
     UA_2 = cell(1, numel(varargin));
     slope_2 = cell(1, numel(varargin));
     shift_2 = cell(1, numel(varargin));
 
     for grp = 1:numel(varargin)
         loadFile = load(fullfile(myPath, varargin{grp}));
-        [fitParam, R_squared] = helperFun(loadFile);
+        [fitParam, R_squared, animal] = helperFun(loadFile);
 
         allRsq{grp} = R_squared;
         UA{grp} = fitParam(:,1);
@@ -96,12 +108,14 @@ elseif contains(varargin{1}, 'LC')
 end
 
 %% Description of helperFun
-    function [fitParam, R_squared] = helperFun(loadFile)
+    function [fitParam, R_squared, animal] = helperFun(loadFile)
         currentFitData = loadFile.trtmntFitData;
         currentFitData = currentFitData(~cellfun(@isempty, currentFitData));
         R_squared = cellfun(@(x) x.R_squared, currentFitData, 'UniformOutput', false);
         R_squared = cell2mat(R_squared);
         fitParam = cellfun(@(x) x.fit_params, currentFitData, 'UniformOutput', false);
         fitParam = cell2mat(fitParam);
+        animal = cellfun(@(x) x.Animal, currentFitData, 'UniformOutput', false);
+        animal = string(animal);
     end
 end
--- Analysis/Clusters/fitParamHistogram.m
+++ Analysis/Clusters/fitParamHistogram.m
@@ -3,26 +3,25 @@
 %
 % Plots histogram of input parameter.
 %
-% Possible parameter names 
+% Possible parameter names
 % logistic3 fit: 'UA', 'slope', 'shift', 'Rsq'
 % logistic4 fit: 'LA', 'slope', 'shift', 'UA', 'Rsq'
 % BC fit: 'LA', 'slope', 'shift', 'UA', 'param_f', 'Rsq'
 % LC fit: 'UA', 'slope', 'shift', 'UA_2', 'slope_2', 'shift_2', 'Rsq'
-% 
+%
 % Invokes allFitParam
 
-function fitParamHistogram(param_name, varargin)
+function fitParamHistogram(param_name, splitbyGender, varargin)
 
-% param_name = 'UA';
-% varargin = {'P2L1 Alcohol_approachavoid_logistic3_fitting_param', ...
-%     'P2L1 Ghrelin_approachavoid_logistic3_fitting_param', ...
-%     'P2L1 Ghr Alcohol_approachavoid_logistic3_fitting_param'};
+% param_name = 'shift'; splitbyGender = 'y';
+% varargin = {'P2L1L3 BL for comb boost and alc_approachavoid_logistic4_fitting_param', ...
+%     'P2L1L3 Boost and alcohol_approachavoid_logistic4_fitting_param'};
 
 if contains(varargin{1}, 'logistic3')
-    [UA, slope, shift, Rsq] = allFitParam(varargin{:});
+    [UA, slope, shift, Rsq, animals] = allFitParam(varargin{:});
 
 elseif contains(varargin{1}, 'logistic4')
-    [LA, slope, shift, UA, Rsq] = allFitParam(varargin{:});
+    [LA, slope, shift, UA, Rsq, animals] = allFitParam(varargin{:});
 
 elseif contains(varargin{1}, 'BC')
     [LA, slope, shift, UA, param_f, Rsq] = allFitParam(varargin{:});
@@ -36,7 +35,7 @@ Colors = parula(numel(varargin));
 
 % param_name_array = {'UA', 'slope', 'shift', 'LA', 'param_f', 'Rsq'};
 % bwArray = [0.05, 10, 0.15, 0.05, 0.05, 0.05]; % Width of bin
-% 
+%
 % index = strcmpi(param_name_array, param_name);
 % currentBW = bwArray(index);
 
@@ -60,6 +59,36 @@ elseif strcmpi(param_name, 'Rsq')
     param_array = Rsq;
 end
 
+% Seperate param_array into male and female
+if strcmpi(splitbyGender, 'y')
+    datasource = 'live_database';
+    conn = database(datasource,'postgres','1234');
+
+    for grp = 1:numel(varargin)
+        animalStr = strjoin(animals{grp}, "','");
+        genderQ = sprintf("SELECT DISTINCT(subjectid), gender FROM live_table WHERE " + ...
+            "subjectid IN ('%s')", animalStr);
+        genderD = fetch(conn, genderQ);
+
+        genderD.subjectid = string(genderD.subjectid);
+        genderD.gender = string(genderD.gender);
+
+        maleAnimals = genderD.subjectid(strcmpi(genderD.gender, 'male'));
+        femaleAnimals = genderD.subjectid(strcmpi(genderD.gender, 'female'));
+
+        maleFilter = ismember(animals{grp}, maleAnimals);
+        femaleFilter = ismember(animals{grp}, femaleAnimals);
+
+        maleData = param_array{grp}(maleFilter);
+        femaleData = param_array{grp}(femaleFilter);
+
+        param_array{grp} = [];
+
+        param_array{grp}{1} = maleData;
+        param_array{grp}{2} = femaleData;
+    end
+end
+
 scriptDir = fileparts(mfilename('fullpath'));
 myPath = fullfile(scriptDir, 'Fig files/');
 
@@ -71,31 +100,61 @@ histOfParam(param_array, param_name);
     function histOfParam(paramArray, param_name)
         for idx = 1:numel(varargin)
 
-%             h(idx) = histogram(paramArray{idx}, 'Normalization','pdf','FaceAlpha',0.7, ...
-%                 'BinWidth', currentBW, 'FaceColor', Colors(idx,:));
+            %             h(idx) = histogram(paramArray{idx}, 'Normalization','pdf','FaceAlpha',0.7, ...
+            %                 'BinWidth', currentBW, 'FaceColor', Colors(idx,:));
 
-            [f, x_values] = ksdensity(paramArray{idx}); % Compute KDE values and corresponding x-values
-            h(idx) = plot(x_values, f, 'LineWidth', 2);
-            % https://www.researchgate.net/publication/320535371_Intelligent_OS_X_malware_threat_detection_with_code_inspection/figures?lo=1
-            % https://stackoverflow.com/questions/46441481/why-does-this-kernel-density-estimation-have-values-over-1-0
-            hold on;
+            if strcmpi(splitbyGender, 'y')
+                subplot(1,2,1);
+                [f_male, x_values_male] = ksdensity(paramArray{idx}{1});
+                h_male(idx) = plot(x_values_male, f_male, 'LineWidth', 2, 'Color', Colors(idx,:));
+                xlabel('value','Interpreter','latex');
+                ylabel('probability density','Interpreter','latex');
+                hold on;
+
+                subplot(1,2,2);
+                [f_female, x_values_female] = ksdensity(paramArray{idx}{2});
+                h_female(idx) = plot(x_values_female, f_female, 'LineWidth', 2, 'Color', Colors(idx,:));
+                xlabel('value','Interpreter','latex');
+                hold on;
+
+            else
+                [f, x_values] = ksdensity(paramArray{idx}); % Compute KDE values and corresponding x-values
+                h(idx) = plot(x_values, f, 'LineWidth', 2, 'Color', Colors(idx,:));
+                hold on;
+            end
         end
 
         hold off;
-        title(sprintf('Histogram of %s', param_name), 'Interpreter','latex','FontSize',25);
-        legend(h, varargin, 'Interpreter','none','Location','best');
-        xlabel('value','Interpreter','latex');
-        ylabel('probability density','Interpreter','latex');
+        sgtitle(sprintf('Histogram of %s', param_name), 'Interpreter','latex','FontSize',25);
+
+        if strcmpi(splitbyGender, 'y')
+            legend(h_female, varargin, 'Interpreter','none','Location','best');
+            linkaxes([subplot(1,2,1), subplot(1,2,2)], 'y');
+        end
+
+        if ~strcmpi(splitbyGender, 'y')
+            legend(h, varargin, 'Interpreter','none','Location','best');
+            xlabel('value','Interpreter','latex');
+            ylabel('probability density','Interpreter','latex');
+        end
 
         if contains(varargin{1}, 'logistic3')
-            figname = sprintf('logistic3_%s_hist_%s', param_name, [varargin{:}]);
+            if strcmpi(splitbyGender, 'y')
+                figname = sprintf('logistic3_%s_hist_%s_MvF', param_name, [varargin{:}]);
+            else
+                figname = sprintf('logistic3_%s_hist_%s', param_name, [varargin{:}]);
+            end
 
         elseif contains(varargin{1}, 'logistic4')
-            figname = sprintf('logistic4_%s_hist_%s', param_name, [varargin{:}]);
+            if strcmpi(splitbyGender, 'y')
+                figname = sprintf('logistic4_%s_hist_%s_MvF', param_name, [varargin{:}]);
+            else
+                figname = sprintf('logistic4_%s_hist_%s', param_name, [varargin{:}]);
+            end
 
         elseif contains(varargin{1}, 'BC')
             figname = sprintf('BC_%s_hist_%s', param_name, [varargin{:}]);
-        
+
         elseif contains(varargin{1}, 'LC')
             figname = sprintf('LC_%s_hist_%s', param_name, [varargin{:}]);
 
@@ -104,4 +163,5 @@ histOfParam(param_array, param_name);
         savefig(gcf, fullfile(myPath, figname));
         close(gcf);
     end
+
 end
