9176e7cdef00cd15235a4859ed8dfb692c2dadf9
atanu <atanurkm11@gmail.com>
2024-08-20 13:32:43 -0600

    Figure generation script

--- Analysis/run_me.m
+++ Analysis/run_me.m
@@ -4,7 +4,7 @@
 fig_directory = '/Users/atanugiri/Downloads/Alcohol Project/Analysis/Fig files/';
 
 %% Figure 1
-% Psychometric plots of individual sessions 
+% Psychometric plots of individual sessions 
 individualPsychometricPlotOverlay('approachavoid', 'n', 'P2L1 BL for comb boost and alc');
 individualPsychometricPlotOverlay('approachavoid', 'n', 'P2L1L3 BL for comb boost and alc');
 individualPsychometricPlotOverlay('approachavoid', 'n', 'P2A Boost and alcohol');
@@ -216,70 +216,103 @@ trajectoryPlot(77530);
 trajectoryPlot(77401);
 
 % Enhanced impact notedduring conflict task
+featureForEach = masterPsychometricFunctionPlot('time_in_feeder_25', 'n', ...
+    'P2L1 BL for comb boost and alc','P2A Boost and alcohol');
+T1 = featureForEach{1};
+T2 = featureForEach{2};
+data = [T1; T2];
+group = [repmat({'T1'}, size(T1,1), 1); repmat({'T2'}, size(T2,1), 1)];
+[d, p, stats] = manova1(data, group);
 
+featureForEach = masterPsychometricFunctionPlot('time_in_feeder_25', 'n', ...
+    'P2L1L3 BL for comb boost and alc','P2A Boost and alcohol');
+T1 = featureForEach{1};
+T2 = featureForEach{2};
+data = [T1; T2];
+group = [repmat({'T1'}, size(T1,1), 1); repmat({'T2'}, size(T2,1), 1)];
+[d, p, stats] = manova1(data, group);
 
-%% ToDo
+% Shift in inflection pointis observed in both tasks
+param_array = fitParamKernelDensity('shift', 'time_in_feeder_25', 2, 'n', ...
+    'P2L1 BL for comb boost and alc', 'P2A Boost and alcohol');
+[h, p] = bootstrap_kstest2(param_array{1}, param_array{2}, 1000);
+fprintf('p = %.4f\n', p);
 
+param_array = fitParamKernelDensity('shift', 'time_in_feeder_25', 2, 'n', ...
+    'P2L1L3 BL for comb boost and alc', 'P2A Boost and alcohol');
+[h, p] = bootstrap_kstest2(param_array{1}, param_array{2}, 1000);
+fprintf('p = %.4f\n', p);
+
+% AA influences time feature in males
 [featureForEachMale, featureForEachFemale] = masterPsychometricFunctionPlot( ...
-    'approachavoid', 'y', ...
-    'P2L1 BL for comb boost and alc','P2L1 Boost and alcohol','P2L1 Post alcohol');
+    'time_in_feeder_25', 'y', 'P2L1 BL for comb boost and alc','P2A Boost and alcohol');
+
+T1 = featureForEachMale{1};
+T2 = featureForEachMale{2};
+data = [T1; T2];
+group = [repmat({'T1'}, size(T1,1), 1); repmat({'T2'}, size(T2,1), 1)];
+[d, p, stats] = manova1(data, group);
+
+T1 = featureForEachFemale{1};
+T2 = featureForEachFemale{2};
+data = [T1; T2];
+group = [repmat({'T1'}, size(T1,1), 1); repmat({'T2'}, size(T2,1), 1)];
+[d, p, stats] = manova1(data, group);
+
+% Shift of inflection pointobserved in males
+[maleParam, femaleParam] = fitParamKernelDensity('shift', 'time_in_feeder_25', ...
+    2, 'y', 'P2L1 BL for comb boost and alc', 'P2A Boost and alcohol');
+
+[h_male, p_male] = bootstrap_kstest2(maleParam{1}, maleParam{2}, 1000);
+[h_female, p_female] = bootstrap_kstest2(femaleParam{1}, femaleParam{2}, 1000);
+
+fprintf('Male: p = %.4f\n', p_male);
+fprintf('Female: p = %.4f\n', p_female);
+
+% AA influencestime in feeder in males
+[featureForEachMale, featureForEachFemale] = masterPsychometricFunctionPlot( ...
+    'time_in_feeder_25', 'y', 'P2L1L3 BL for comb boost and alc','P2A Boost and alcohol');
+
+T1 = featureForEachMale{1};
+T2 = featureForEachMale{2};
+data = [T1; T2];
+group = [repmat({'T1'}, size(T1,1), 1); repmat({'T2'}, size(T2,1), 1)];
+[d, p, stats] = manova1(data, group);
+
+T1 = featureForEachFemale{1};
+T2 = featureForEachFemale{2};
+data = [T1; T2];
+group = [repmat({'T1'}, size(T1,1), 1); repmat({'T2'}, size(T2,1), 1)];
+[d, p, stats] = manova1(data, group);
+
+% Shift of inflection pointstronger in males
+[maleParam, femaleParam] = fitParamKernelDensity('shift', 'time_in_feeder_25', ...
+    2, 'y', 'P2L1L3 BL for comb boost and alc', 'P2A Boost and alcohol');
+
+[h_male, p_male] = bootstrap_kstest2(maleParam{1}, maleParam{2}, 1000);
+[h_female, p_female] = bootstrap_kstest2(femaleParam{1}, femaleParam{2}, 1000);
+
+fprintf('Male: p = %.4f\n', p_male);
+fprintf('Female: p = %.4f\n', p_female);
 
 
-%% 4-param logistic fit plots
-[maleParam, femaleParam] = fitParamKernelDensity('shift', 'approachavoid', 2, 'y', ...
-    'P2L1 BL for comb boost and alc', 'P2L1 Boost and alcohol');
+%% Figure 4
+% Psychometric profiles of a vulnerable animal: AA impact
+individualPsychPlotPerSession('approachavoid', ...
+    'P2L1 BL for comb boost and alc', 'sully');
+individualPsychPlotPerSession('approachavoid', ...
+    'P2L1L3 BL for comb boost and alc', 'sully');
+individualPsychPlotPerSession('approachavoid', ...
+    'P2A Boost and alcohol', 'sully');
 
+individualPsychPlotPerSession('approachavoid', ...
+    'P2L1 BL for comb boost and alc', 'shakira');
+individualPsychPlotPerSession('approachavoid', ...
+    'P2L1L3 BL for comb boost and alc', 'shakira');
+individualPsychPlotPerSession('approachavoid', ...
+    'P2A Boost and alcohol', 'shakira');
 
-
-
-%% Chi-sqaure test on pie chart
-param = {'LA', 'slope', 'shift', 'UA'};
-for i = 1:numel(param)
-    [ct_in_part_L1_BL, totalNonSig_L1_BL] = pieChartPlot( ...
-        'P2L1 BL for comb boost and alc_approachavoid_logistic4_fitting_param.mat', param{i});
-
-    [ct_in_part_L1_Alc, totalNonSig_L1_Alc] = pieChartPlot( ...
-        'P2L1 Boost and alcohol_approachavoid_logistic4_fitting_param.mat', param{i});
-
-    [ct_in_part_L1L3_BL, totalNonSig_L1L3_BL] = pieChartPlot( ...
-        'P2L1L3 BL for comb boost and alc_approachavoid_logistic4_fitting_param.mat', param{i});
-
-    [ct_in_part_L1L3_Alc, totalNonSig_L1L3_Alc] = pieChartPlot( ...
-        'P2L1L3 Boost and alcohol_approachavoid_logistic4_fitting_param.mat', param{i});
-
-    if i <= 1
-        group_L1_BL = [ct_in_part_L1_BL, totalNonSig_L1_BL];
-        group_L1_Alc = [ct_in_part_L1_Alc, totalNonSig_L1_Alc];
-
-        group_L1L3_BL = [ct_in_part_L1L3_BL, totalNonSig_L1L3_BL];
-        group_L1L3_Alc = [ct_in_part_L1L3_Alc, totalNonSig_L1L3_Alc];
-
-    else
-        group_L1_BL = [group_L1_BL, ct_in_part_L1_BL];
-        group_L1_Alc = [group_L1_Alc, ct_in_part_L1_Alc];
-
-        group_L1L3_BL = [group_L1L3_BL, ct_in_part_L1L3_BL];
-        group_L1L3_Alc = [group_L1L3_Alc, ct_in_part_L1L3_Alc];
-
-    end
-end
-
-contingencyTable = [group_L1_BL; group_L1_Alc];
-
-% Use chi2cont from MATLAB File Exchange
-[chi2, p, df] = chi2cont(contingencyTable);
-
-% Display the results
-disp('Chi-Square Statistic:');
-disp(chi2);
-disp('p-value:');
-disp(p);
-
-
-%% Variance analysis
-
-
-%% Individual difference (07/26/2024)
+% Comparing sigmoid fractions in AA: male vs. female
 [count1, total1] = calculateFractionOfSigmoid('male', 'approachavoid', 2, ...
     'P2L1 Boost and alcohol');
 figure;
@@ -294,68 +327,44 @@ p = chi2test([count1, (total1 - count1); count2, (total2 - count2)]);
 fprintf('p = %.4f\n', p);
 
 
-%% Individual difference (07/11/2024)
-% Define the male names and treatment groups
-% males = {'aladdin', 'carl', 'jafar', 'jimi', 'jr', 'kobe', 'mike', 'scar', 'simba', 'sully'};
-females = {'alexis', 'fiona', 'harley', 'juana', 'kryssia', 'neftali', 'raven', 'renata', 'sarah', 'shakira'};
-treatmentGrps = {'P2L1 BL for comb boost and alc', 'P2L1L3 BL for comb boost and alc', ...
-    'P2A Boost and alcohol'};
+%% Figure 5
+% Psychometric plots of individual sessions
+individualPsychometricPlotOverlay('approachavoid', 'n', 'P2L1 Boost and alcohol');
+individualPsychometricPlotOverlay('approachavoid', 'n', 'P2L1L3 Boost and alcohol');
 
-% Initialize a cell array to store the feature lists
-featureLists = cell(numel(treatmentGrps), numel(females));
+individualPsychometricPlotOverlay('approachavoid', 'n', 'P2L1 Post alcohol');
+individualPsychometricPlotOverlay('approachavoid', 'n', 'P2L1L3 Post alcohol');
 
-nrows = size(featureLists, 1);
-ncols = size(featureLists, 2);
+% Shift of inflection pointobserved only in PC
+param_array = fitParamKernelDensity('shift', 'approachavoid', 2, 'n', ...
+    'P2L1 BL for comb boost and alc', 'P2L1 Boost and alcohol', 'P2L1 Post alcohol');
+[h, p] = bootstrap_kstest2(param_array{1}, param_array{2}, 1000);
+fprintf('p = %.4f\n', p);
+[h, p] = bootstrap_kstest2(param_array{1}, param_array{3}, 1000);
+fprintf('p = %.4f\n', p);
 
-parfor grp = 1:nrows
-    tempRow = cell(1, ncols);
+param_array = fitParamKernelDensity('shift', 'approachavoid', 2, 'n', ...
+    'P2L1L3 BL for comb boost and alc', 'P2L1L3 Boost and alcohol', 'P2L1L3 Post alcohol');
+[h, p] = bootstrap_kstest2(param_array{1}, param_array{2}, 1000);
+fprintf('p = %.4f\n', p);
+[h, p] = bootstrap_kstest2(param_array{1}, param_array{3}, 1000);
+fprintf('p = %.4f\n', p);
 
-    for animal = 1:ncols
-        tempRow{animal} = individualPsychValuesPerSession('approachavoid', ...
-            treatmentGrps{grp}, females{animal});
-    end
+% Greater influence onapproach rate in conflict task
+featureForEach = masterPsychometricFunctionPlot('approachavoid', 'n', ...
+    'P2L1 BL for comb boost and alc', 'P2L1 Boost and alcohol', 'P2L1 Post alcohol');
+T1 = featureForEach{1};
+T2 = featureForEach{2};
+T3 = featureForEach{3};
+data = [T1; T2];
+group = [repmat({'T1'}, size(T1,1), 1); repmat({'T2'}, size(T2,1), 1)];
+[d, p, stats] = manova1(data, group);
 
-    featureLists(grp, :) = tempRow;
-end
+data = [T1; T3];
+group = [repmat({'T1'}, size(T1,1), 1); repmat({'T3'}, size(T3,1), 1)];
+[d, p, stats] = manova1(data, group);
 
-
-%% Cluster for individual difference (07/17/2024)
-males = {'aladdin', 'carl', 'jafar', 'jimi', 'jr', 'kobe', 'mike', 'scar', ...
-    'simba', 'sully'};
-% males = {'alexis', 'fiona', 'harley', 'juana', 'kryssia', 'neftali', ...
-%     'raven', 'renata', 'sarah', 'shakira'};
-
-featureList = {'LA', 'slope', 'shift', 'UA', 'Rsq'};
-treatmentGroups = {'P2L1 BL for comb boost and alc_approachavoid_logistic4_fitting_param.mat', ...
-    'P2A Boost and alcohol_approachavoid_logistic4_fitting_param.mat', ...
-    'P2L1L3 BL for comb boost and alc_approachavoid_logistic4_fitting_param.mat'};
-
-maleParams = cell(1, numel(treatmentGroups));
-stdErrMales = cell(1, numel(treatmentGroups));
-
-% Initialize each cell as a struct with feature names
-for grp = 1:numel(treatmentGroups)
-    maleParams{grp} = struct();
-    stdErrMales{grp} = struct();
-    for featureIdx = 1:numel(featureList)
-        feature = featureList{featureIdx};
-        maleParams{grp}.(feature) = [];
-        stdErrMales{grp}.(feature) = [];
-    end
-end
+% Conflict task exhibits greater variance change
 
 
-for featureIdx = 1:numel(featureList)
-    feature = featureList{featureIdx};
-    [~, maleParam, stdErrMale] = indivAnimalParamCompPlot(feature, 'y', ...
-        treatmentGroups{:});
-    %     [~, ~, ~, ~, maleParam, stdErrMale] = indivAnimalParamCompPlot(feature, 'y', ...
-    %         treatmentGroups{:});
-
-    for grp = 1:numel(treatmentGroups)
-        maleParams{grp}.(feature) = maleParam(:, grp);
-        stdErrMales{grp}.(feature) = stdErrMale(:, grp);
-    end
-end
-
-featureBiplotForIndivAnimal(featureList, males, treatmentGroups, maleParams);
+%% ToDo
